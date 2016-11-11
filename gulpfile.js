a425f085ab4285afdd9d6f647b7caa5ea884cb0a
Nabil Kashyap <nkashya1@swarthmore.edu>
2016-11-11 02:14:44 -0500

    implemented perfect-scrollbar

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
     cp = require('child_process').exec,
+    uglify = require('gulp-uglify'),
     bs = require('browser-sync').create();
 
-var src = 'assets',
-    dest = '_site/assets';
+var src = './assets',
+    dest = './_site/assets';
 
 // Task for building blog when something changed:
 gulp.task('jekyll', function(){
-    cp('bundle exec jekyll build --watch',
+    cp('exec jekyll build --incremental --watch',
         function(err,stdout,stderr){
             console.log(err);
             console.log(stdout);
@@ -20,19 +23,24 @@ gulp.task('jekyll', function(){
 gulp.task('serve', function () {
     bs.init({server: {baseDir: '_site/'}, port: 4000});
     // Reloads page when some of the already built files changed:
-    gulp.watch('_site/**/*.*').on('change', bs.reload);
+    // gulp.watch('_site/**/*.*').on('change', bs.reload);
 });
 
-// gulp.task('js', function(){
-//     return gulp.src( src + '/js/app.js' )
-//         .pipe(browserify({
-//             transform: 'reactify',
-//             debug: 'true'
-//         }))
-//         .on('error', function(err) {
-//             console.error('NOPE! ', err.message);
-//         })
-//         .pipe(gulp.dest( dest + '/js' ))
-// });
+gulp.task('js', function(){
 
-gulp.task('default', ['jekyll', 'serve']);
+    browserify({
+        entries: src + '/js/main.js',
+        debug: true
+    })
+        .bundle()
+        .pipe(source('main.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(dest + '/js'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch(src + '/js/*.js', ['js']);
+});
+
+gulp.task('default', [ 'js','watch','jekyll', 'serve']);
